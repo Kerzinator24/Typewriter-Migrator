630bd9f916b508b283a7574a8d3dfb4eb1a504e0
Kerzinator_24 <kerzinator24@gmail.com>
2024-04-25 11:59:03 +0200

    removed unneeded files

--- migrateAll.js
+++
@@ -1,7 +0,0 @@
-// Function to handle migration of all NPCs
-function migrateAllNpcs(selectedModules) {
-  selectedModules.forEach(function(module) {
-    // Implement migration logic for all NPCs here
-    console.log("Migrating all NPCs for module:", module);
-  });
-}
